96be893bbfecec2b8ef5bb968fd9db921158b29c
Ananya <ananyaganesh01@gmail.com>
2019-02-23 06:09:36 +0530

    Api commit

---
+++ API/crud.py
@@ -0,0 +1,26 @@
+# endpoint to update user
+@app.route("/user/<id>", methods=["PUT"])
+def user_update(id):
+    user = User.query.get(id)
+    username = request.json['username']
+    email = request.json['email']
+
+    user.email = email
+    user.username = username
+
+    db.session.commit()
+    return user_schema.jsonify(user)
+
+
+# endpoint to delete user
+@app.route("/user/<id>", methods=["DELETE"])
+def user_delete(id):
+    user = User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+
+    return user_schema.jsonify(user)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
